498338bb7ff0773a67d28152c30903beb339cc3d
Jamie Copeland <jamiecopeland10@gmail.com>
2020-05-25 14:07:09 -0400

    Zone kml data for all zones has been added.

--- app/src/main/java/android/example/ontariofish/MapsActivity.java
+++ app/src/main/java/android/example/ontariofish/MapsActivity.java
@@ -58,14 +58,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         KmlLayer zone2 = addLayer(R.raw.zone2);
         KmlLayer zone3 = addLayer(R.raw.zone3);
         KmlLayer zone4 = addLayer(R.raw.zone4);
-
+        KmlLayer zone5 = addLayer(R.raw.zone5);
+        KmlLayer zone6 = addLayer(R.raw.zone6);
+        KmlLayer zone7 = addLayer(R.raw.zone7);
+        KmlLayer zone8 = addLayer(R.raw.zone8);
+        KmlLayer zone9 = addLayer(R.raw.zone9);
+        KmlLayer zone10 = addLayer(R.raw.zone10);
+        KmlLayer zone11 = addLayer(R.raw.zone11);
+        KmlLayer zone12 = addLayer(R.raw.zone12);
+        KmlLayer zone13 = addLayer(R.raw.zone13);
+        KmlLayer zone14 = addLayer(R.raw.zone14);
+        KmlLayer zone15 = addLayer(R.raw.zone15);
+        KmlLayer zone16 = addLayer(R.raw.zone16);
+        KmlLayer zone18 = addLayer(R.raw.zone18);
+        KmlLayer zone19 = addLayer(R.raw.zone19);
+        KmlLayer zone17 = addLayer(R.raw.zone17);
+        KmlLayer zone20 = addLayer(R.raw.zone20);
         zone1.addLayerToMap();
         zone2.addLayerToMap();
         zone3.addLayerToMap();
         zone4.addLayerToMap();
-
-
-
+        zone5.addLayerToMap();
+        zone6.addLayerToMap();
+        zone7.addLayerToMap();
+        zone8.addLayerToMap();
+        zone9.addLayerToMap();
+        zone10.addLayerToMap();
+        zone11.addLayerToMap();
+        zone12.addLayerToMap();
+        zone13.addLayerToMap();
+        zone14.addLayerToMap();
+        zone15.addLayerToMap();
+        zone16.addLayerToMap();
+        zone18.addLayerToMap();
+        zone19.addLayerToMap();
+        zone17.addLayerToMap();
+        zone20.addLayerToMap();
         mMap.setLatLngBoundsForCameraTarget(OntarioRestrict);
 
         UiSettings uiSettings = mMap.getUiSettings();
