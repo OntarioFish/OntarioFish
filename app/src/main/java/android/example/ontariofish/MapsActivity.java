22317b8d2f829358b8402777f1e611c3602a9d80
Jamie Copeland <jamiecopeland10@gmail.com>
2020-05-25 21:32:55 -0400

    Testing geojson instead

--- app/src/main/java/android/example/ontariofish/MapsActivity.java
+++ app/src/main/java/android/example/ontariofish/MapsActivity.java
@@ -3,6 +3,8 @@ package android.example.ontariofish;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,8 +14,12 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.data.Feature;
+import com.google.maps.android.data.geojson.GeoJsonLayer;
 import com.google.maps.android.data.kml.KmlLayer;
+import com.google.maps.android.data.kml.KmlPolygon;
 
+import org.json.JSONException;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
@@ -52,62 +58,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng Ontario = new LatLng(51.833568, -86.997632);
         LatLng Southwest = new LatLng(41.721325, -95.150434);
         LatLng NorthEast = new LatLng(57.910221, -74.343067);
-        LatLngBounds OntarioRestrict = new LatLngBounds(Southwest, NorthEast);
+        //LatLngBounds OntarioRestrict = new LatLngBounds(Southwest, NorthEast);
 
-        KmlLayer zone1 = addLayer(R.raw.zone1);
-        KmlLayer zone2 = addLayer(R.raw.zone2);
-        KmlLayer zone3 = addLayer(R.raw.zone3);
-        KmlLayer zone4 = addLayer(R.raw.zone4);
-        KmlLayer zone5 = addLayer(R.raw.zone5);
-        KmlLayer zone6 = addLayer(R.raw.zone6);
-        KmlLayer zone7 = addLayer(R.raw.zone7);
-        KmlLayer zone8 = addLayer(R.raw.zone8);
-        KmlLayer zone9 = addLayer(R.raw.zone9);
-        KmlLayer zone10 = addLayer(R.raw.zone10);
-        KmlLayer zone11 = addLayer(R.raw.zone11);
-        KmlLayer zone12 = addLayer(R.raw.zone12);
-        KmlLayer zone13 = addLayer(R.raw.zone13);
-        KmlLayer zone14 = addLayer(R.raw.zone14);
-        KmlLayer zone15 = addLayer(R.raw.zone15);
-        KmlLayer zone16 = addLayer(R.raw.zone16);
-        KmlLayer zone18 = addLayer(R.raw.zone18);
-        KmlLayer zone19 = addLayer(R.raw.zone19);
-        KmlLayer zone17 = addLayer(R.raw.zone17);
-        KmlLayer zone20 = addLayer(R.raw.zone20);
-        zone1.addLayerToMap();
-        zone2.addLayerToMap();
-        zone3.addLayerToMap();
-        zone4.addLayerToMap();
-        zone5.addLayerToMap();
-        zone6.addLayerToMap();
-        zone7.addLayerToMap();
-        zone8.addLayerToMap();
-        zone9.addLayerToMap();
-        zone10.addLayerToMap();
-        zone11.addLayerToMap();
-        zone12.addLayerToMap();
-        zone13.addLayerToMap();
-        zone14.addLayerToMap();
-        zone15.addLayerToMap();
-        zone16.addLayerToMap();
-        zone18.addLayerToMap();
-        zone19.addLayerToMap();
-        zone17.addLayerToMap();
-        zone20.addLayerToMap();
-        mMap.setLatLngBoundsForCameraTarget(OntarioRestrict);
+       // KmlLayer zone1 = addLayer(R.raw.zone1);
+   //     KmlLayer work = addLayer(R.raw.plzwork);
+//        KmlLayer zone2 = addLayer(R.raw.zone2);
+//        KmlLayer zone3 = addLayer(R.raw.zone3);
+//        KmlLayer zone4 = addLayer(R.raw.zone4);
+//        KmlLayer zone5 = addLayer(R.raw.zone5);
+//        KmlLayer zone6 = addLayer(R.raw.zone6);
+//        KmlLayer zone7 = addLayer(R.raw.zone7);
+//        KmlLayer zone8 = addLayer(R.raw.zone8);
+//        KmlLayer zone9 = addLayer(R.raw.zone9);
+//        KmlLayer zone10 = addLayer(R.raw.zone10);
+//        KmlLayer zone11 = addLayer(R.raw.zone11);
+//        KmlLayer zone12 = addLayer(R.raw.zone12);
+//        KmlLayer zone13 = addLayer(R.raw.zone13);
+//        KmlLayer zone14 = addLayer(R.raw.zone14);
+//        KmlLayer zone15 = addLayer(R.raw.zone15);
+//        KmlLayer zone16 = addLayer(R.raw.zone16);
+//        KmlLayer zone18 = addLayer(R.raw.zone18);
+//        KmlLayer zone19 = addLayer(R.raw.zone19);
+//        KmlLayer zone17 = addLayer(R.raw.zone17);
+//        KmlLayer zone20 = addLayer(R.raw.zone20);
+ //       zone1.addLayerToMap();
+//        work.addLayerToMap();
+//        zone2.addLayerToMap();
+//        zone3.addLayerToMap();
+//        zone4.addLayerToMap();
+//        zone5.addLayerToMap();
+//        zone6.addLayerToMap();
+//        zone7.addLayerToMap();
+//        zone8.addLayerToMap();
+//        zone9.addLayerToMap();
+//        zone10.addLayerToMap();
+//        zone11.addLayerToMap();
+//        zone12.addLayerToMap();
+//        zone13.addLayerToMap();
+//        zone14.addLayerToMap();
+//        zone15.addLayerToMap();
+//        zone16.addLayerToMap();
+//        zone18.addLayerToMap();
+//        zone19.addLayerToMap();
+//        zone17.addLayerToMap();
+//        zone20.addLayerToMap();
+        //mMap.setLatLngBoundsForCameraTarget(OntarioRestrict);
 
+
+        GeoJsonLayer testing  = addLayer(R.raw.plztest);
+    //    testing.addLayerToMap();
         UiSettings uiSettings = mMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(true);
 
-        mMap.addMarker(new MarkerOptions().position(Ontario).title("Marker in Ontario"));
+//        mMap.addMarker(new MarkerOptions().position(Ontario).title("Marker in Ontario"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Ontario));
+
+
+
+
+
+
     }
 
-    public KmlLayer addLayer(int resourceId){
+    public GeoJsonLayer addLayer(int resourceId){
 
         try {
-            return new KmlLayer(mMap, resourceId, this);
-        } catch (XmlPullParserException | IOException e) {
+            return new GeoJsonLayer(mMap, resourceId, this);
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
 
