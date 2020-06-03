7fc0486a97acabdbb4965bafc59e00271cb05eb6
Jamie <jamiecopeland10@gmail.com>
2020-06-03 12:06:39 -0400

    Working towards making seasons and limits pop up for selected fish from dropdown menu.

--- app/src/main/java/android/example/ontariofish/FishRegulations.java
+++ app/src/main/java/android/example/ontariofish/FishRegulations.java
@@ -4,31 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FishRegulations extends AppCompatActivity {
+public class FishRegulations extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private TextView zoneTitle;
     private Spinner fishSelect;
+    private int regionNumber;
 
-    DatabaseHelper DB;
-
-    public List<String> regulationFish= new ArrayList<>();
-    public List<String> regulationFishTemp= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fish_regulations);
 
-        DB = new DatabaseHelper(this);
+        List<String> regulationFish= new ArrayList<>();
 
         Window window = getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this,R.color.statusBarColor));
@@ -40,107 +40,133 @@ public class FishRegulations extends AppCompatActivity {
         fishSelect = (Spinner)findViewById(R.id.fish_spinner);
         regulationFish = setTitle(zoneName);
 
-        /*
-            Below array list is used for testing. It works with the array list created below
-         */
-        ArrayList<String> testing =  new ArrayList<String>();
-        testing.add("herro");
-        testing.add("YES");
-
-        //This system.out crashes the app. Pretty sure it is out of bounds or something.
         System.out.println(regulationFish.get(0));
-        //Replace the last word in the line testing with regulationFish and it should be working but doesnt for me.
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, testing);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, regulationFish);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         fishSelect.setAdapter(adapter);
-
+        fishSelect.setOnItemSelectedListener(this);
     }
 
     public List<String> setTitle(String zoneName){
+        DatabaseHelper DB;
+        DB = new DatabaseHelper(this);
+        List<String> regulationFishTemp= new ArrayList<>();
         switch(zoneName){
             case "#zone1":
                 zoneTitle.setText("Zone 1");
                 regulationFishTemp = DB.getRegulationsFish("1");
+                regionNumber = 1;
                 break;
             case "#zone2":
                 zoneTitle.setText("Zone 2");
                 regulationFishTemp = DB.getRegulationsFish("2");
+                regionNumber = 2;
                 break;
             case "#zone3":
                 zoneTitle.setText("Zone 3");
                 regulationFishTemp = DB.getRegulationsFish("3");
+                regionNumber = 3;
                 break;
             case "#zone4":
                 zoneTitle.setText("Zone 4");
                 regulationFishTemp = DB.getRegulationsFish("4");
+                regionNumber = 4;
                 break;
             case "#zone5":
                 zoneTitle.setText("Zone 5");
                 regulationFishTemp = DB.getRegulationsFish("5");
+                regionNumber = 5;
                 break;
             case "#zone6":
                 zoneTitle.setText("Zone 6");
                 regulationFishTemp = DB.getRegulationsFish("6");
+                regionNumber = 6;
                 break;
             case "#zone7":
                 zoneTitle.setText("Zone 7");
                 regulationFishTemp = DB.getRegulationsFish("7");
+                regionNumber = 7;
                 break;
             case "#zone8":
                 zoneTitle.setText("Zone 8");
                 regulationFishTemp = DB.getRegulationsFish("8");
+                regionNumber = 8;
                 break;
             case "#zone9":
                 zoneTitle.setText("Zone 9");
                 regulationFishTemp = DB.getRegulationsFish("9");
+                regionNumber = 9;
                 break;
             case "#zone10":
                 zoneTitle.setText("Zone 10");
                 regulationFishTemp = DB.getRegulationsFish("10");
+                regionNumber = 10;
                 break;
             case "#zone11":
                 zoneTitle.setText("Zone 11");
                 regulationFishTemp = DB.getRegulationsFish("11");
+                regionNumber = 11;
                 break;
             case "#zone12":
                 zoneTitle.setText("Zone 12");
                 regulationFishTemp = DB.getRegulationsFish("12");
+                regionNumber = 12;
                 break;
             case "#zone13":
                 zoneTitle.setText("Zone 13");
                 regulationFishTemp = DB.getRegulationsFish("13");
+                regionNumber = 13;
                 break;
             case "#zone14":
                 zoneTitle.setText("Zone 14");
                 regulationFishTemp = DB.getRegulationsFish("14");
+                regionNumber = 14;
                 break;
             case "#zone15":
                 zoneTitle.setText("Zone 15");
                 regulationFishTemp = DB.getRegulationsFish("15");
+                regionNumber = 15;
                 break;
             case "#zone16":
                 zoneTitle.setText("Zone 16");
                 regulationFishTemp = DB.getRegulationsFish("16");
+                regionNumber = 16;
                 break;
             case "#zone17":
                 zoneTitle.setText("Zone 17");
                 regulationFishTemp = DB.getRegulationsFish("17");
+                regionNumber = 17;
                 break;
             case "#zone18":
                 zoneTitle.setText("Zone 18");
                 regulationFishTemp = DB.getRegulationsFish("18");
+                regionNumber = 18;
                 break;
             case "#zone19":
                 zoneTitle.setText("Zone 19");
                 regulationFishTemp = DB.getRegulationsFish("19");
+                regionNumber = 19;
                 break;
             case "#zone20":
                 zoneTitle.setText("Zone 20");
                 regulationFishTemp = DB.getRegulationsFish("20");
+                regionNumber = 20;
                 break;
         }
 
         return regulationFishTemp;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(FishRegulations.this, (String)parent.getItemAtPosition(position), Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
