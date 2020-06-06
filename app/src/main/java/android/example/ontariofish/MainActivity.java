f117766687d73fe0494603f9e817bc6e45859ca3
Zack <zurbaniak@rogers.com>
2020-06-06 12:50:03 -0400

    Remove Background Video

--- app/src/main/java/android/example/ontariofish/MainActivity.java
+++ app/src/main/java/android/example/ontariofish/MainActivity.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private VideoView mVideoView;
+
     private Button mFishInfoButton, mViewRegsButton,mMapsButton;
 
     public List<FishSample> fishSamples= new ArrayList<>();
@@ -41,13 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
         mFishInfoButton = (Button)findViewById(R.id.view_fish);
         mMapsButton = (Button)findViewById(R.id.view_map);
-        mVideoView = (VideoView) findViewById(R.id.background_video);
         mViewRegsButton = (Button) findViewById(R.id.regulations_button);
         Window window = getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this,R.color.statusBarColor));
 
-        Uri uri = Uri.parse("android.resource://"+ getPackageName() + "/" + R.raw.output);
-        mVideoView.setVideoURI(uri);
 
         /* Creates a test Array to see if the database has already been created,
          in order to avoid needless processing */
@@ -56,13 +53,6 @@ public class MainActivity extends AppCompatActivity {
             readDataFishInfo();
         }
 
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.setLooping(true);
-                mp.setVolume(0,0);
-            }
-        });
 
         mFishInfoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,11 +77,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mVideoView.start();
-    }
 
     /* Function reads lines of data from fishinfo.txt, which can be found in the "raw" folder
     under the resource files folder */
