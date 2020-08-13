65725f0ba025f4623ff04306a45acfde8f6664fa
Zack <zurbaniak@rogers.com>
2020-08-13 17:32:27 -0400

    changed log entry date picking to a datepickerdialog

--- app/src/main/java/android/example/ontariofish/LogbookEntryDialog.java
+++ app/src/main/java/android/example/ontariofish/LogbookEntryDialog.java
@@ -1,15 +1,19 @@
 package android.example.ontariofish;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -23,12 +27,15 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Objects;
 
 public class LogbookEntryDialog extends AppCompatDialogFragment {
 
     private LogbookDialogListener listener;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
     ArrayList<MyEntry> mEntryList;
+
 
     @NonNull
     @Override
@@ -46,17 +53,41 @@ public class LogbookEntryDialog extends AppCompatDialogFragment {
         LayoutInflater inflater = Objects.requireNonNull(getActivity()).getLayoutInflater();
         final View view = inflater.inflate(R.layout.logbook_add_entry_dialog, null);
 
+        final Spinner line1 = view.findViewById(R.id.fish_spinner2);
+        final EditText line2 = view.findViewById(R.id.editTextLocation2);
+        final EditText line3 = view.findViewById(R.id.editTextSize2);
+        final EditText line4 = view.findViewById(R.id.editTextWeight2);
+        final TextView line5 = view.findViewById(R.id.editTextDate2);
+
+        line5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        Objects.requireNonNull(getActivity()), android.R.style.Theme_Holo_Light_Dialog, mDateSetListener, year, month, day);
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month +1;
+                String date = month + "/" + day + "/" + year;
+                line5.setText(date);
+            }
+        };
+
         builder.setView(view)
                 .setPositiveButton("Save", new DialogInterface.OnClickListener() {
 
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Spinner line1 = view.findViewById(R.id.fish_spinner2);
-                        EditText line2 = view.findViewById(R.id.editTextLocation2);
-                        EditText line3 = view.findViewById(R.id.editTextSize2);
-                        EditText line4 = view.findViewById(R.id.editTextWeight2);
-                        EditText line5 = view.findViewById(R.id.editTextDate2);
-
 
                         if (!line1.getSelectedItem().toString().matches("") ||
                                 !line2.getText().toString().matches("") ||
